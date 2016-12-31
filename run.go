d3cac0610b17df7b10e91800e3a1d3c39f71bc99
wactbprot <thsteinbock@web.de>
2016-12-31 14:50:20 +0100

    ..

--- run.go
+++ run.go
@@ -1,14 +1,15 @@
 package main
+
 import "github.com/wactbprot/m/ini"
 
 func main() {
 
 	config := ini.ParseConfig(ini.GetConfig("config.json"))
-	ds := ini.GetDataClient(config)
 
+	ds := ini.GetDataClient(config)
 	ini.TestDataClient(ds)
 	ini.UploadConfig(ds, config)
- 
+
 	db := ini.GetDbClient(config)
 	ini.TestDbClient(db)
 }
